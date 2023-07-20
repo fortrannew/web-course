de6f232f4b03562b3951048fd56a367038210300
Ivan MacOS <fortran.new@gmail.com>
2023-07-21 04:16:31 +0700

    Добавление условий использования платформы

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "11e8e5bd9dbda1f8f1e122d98a8ad32a",
-"index.html": "a4798b0bf551517e79b9b1701c500ad9",
-"/": "a4798b0bf551517e79b9b1701c500ad9",
-"main.dart.js": "cbb19fff76d58bd2f3630c8c7524f653",
+"index.html": "67601b548ed089b819e85f79594d507d",
+"/": "67601b548ed089b819e85f79594d507d",
+"main.dart.js": "991160152c65da169ee6b9e24affae35",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
